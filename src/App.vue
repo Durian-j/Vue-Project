50c88a2e8968d38d14bfe6e5e17ebebb6d6d20e7
琴南 <1044009028@qq.com>
2018-01-03 20:03:47 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,28 @@
         <mt-header fixed title="Vue 商城项目"></mt-header>
 
         <!-- 中间的路由 router-view 区域 -->
-        <!-- <router-view></router-view> -->
+        <transition>
+            <router-view></router-view>
+        </transition>
         
         <!-- 底部 Tabbar 区域 -->
         <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</a>
+			</router-link>
 		</nav>
     </div>
 </template>
@@ -35,5 +37,20 @@
 <style lang="scss" scoped>
 	.app-container{
 		padding-top:40px;
+		background-color: #fff;
+		overflow-x:hidden;
+	}
+	.v-enter{
+		opacity:0;
+		transform:translateX(100%);
+	}
+	.v-leave-to{
+		opacity:0;
+		transform:translateX(-100%);
+		position: absolute;
+	}
+	.v-enter-active,
+	.v-leave-active{
+		transition:all 0.5s ease;
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,30 @@
 // 导入vue并实例化
 import Vue from 'vue';
 
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import router from './router.js'
+
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
 //导入跟组件用render函数进行解析
 import App from './App.vue';
 //导入mint-ui和mui
-import {Header} from 'mint-ui';
+import {Header,Swipe,SwipeItem} from 'mint-ui';
 //按需引入
 Vue.component(Header.name,Header);
+Vue.component(Swipe.name,Swipe);
+Vue.component(SwipeItem.name,SwipeItem);
 
 import './lib/mui/css/mui.css';
+import './lib/mui/css/icons-extra.css';
 
 var vm=new Vue({
 	el:'#app',
-	render:c=>c(App)
+	render:c=>c(App),
+	router
 })
